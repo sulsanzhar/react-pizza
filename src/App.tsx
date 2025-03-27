9410f68b4f8e62da00b6bb535893eebcd12e0c4f
Sultanov Sanzhar <sulsanzhar@mail.ru>
2025-03-27 19:04:37 +0500

    added beautiful pizza loader and slightly improved performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import Header from "./components/Header.tsx";
-import Home from './pages/Home.tsx';
 import { Route, Routes } from "react-router-dom";
-import Cart from "./pages/Cart.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import { lazy, Suspense } from "react";
+import Loader from './components/Loader/Loader.tsx';
 import './scss/app.scss';
+
+const Home = lazy(() => import("./pages/Home.tsx"));
+const Cart = lazy(() => import("./pages/Cart.tsx"));
+const NotFound = lazy(() => import("./pages/NotFound.tsx"));
 
 function App() {
   return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Suspense fallback={<Loader />}>
           <Routes>
             <Route index path="/" element={<Home />} />
             <Route path="/Cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </div>
+        </Suspense>
       </div>
+    </div>
   );
 }
 
