88d1c4c3bb2ac7c85c9acbc3514c81a2e572822a
Sultanov Sanzhar <sulsanzhar@mail.ru>
2025-03-27 15:41:14 +0500

    slightly modified page not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
+import notFound from '../assets/img/404-error.webp';
 import "./NotFound.scss";
 
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h1>404</h1>
-      <div className="error-message">Страница не найдена</div>
-      <p>Кажется, вы заблудились. Попробуйте вернуться назад.</p>
+      <img src={notFound} alt="" />
+      <p>К сожалению данная страница отсутствует в нашем интернет-магазине</p>
       <Link to="/" className="back-home">
         Вернуться на главную
       </Link>
